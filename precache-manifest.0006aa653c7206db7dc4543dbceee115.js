ad07d0963598a1418b794083711fc3b338d8052a
Sam Pita <samanthakpita@gmail.com>
2020-02-21 16:34:11 -0600

    Updates

---
+++ precache-manifest.0006aa653c7206db7dc4543dbceee115.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3239504940a87e0833a8e82b0c997484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dd9839f27fe8a2a51a6",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "02d571d464ec993d7014",
+    "url": "/static/css/main.7a2300c5.chunk.css"
+  },
+  {
+    "revision": "1dd9839f27fe8a2a51a6",
+    "url": "/static/js/2.c7dfd80a.chunk.js"
+  },
+  {
+    "revision": "02d571d464ec993d7014",
+    "url": "/static/js/main.52f3294c.chunk.js"
+  },
+  {
+    "revision": "6cb34a6bbe1e8268a0e8",
+    "url": "/static/js/runtime-main.a9078ebf.js"
+  },
+  {
+    "revision": "7a6dbc2646fceaa5383f17ce9d617ac3",
+    "url": "/static/media/36logo.7a6dbc26.png"
+  },
+  {
+    "revision": "82c705a4714ee5b5f38f2b30ba4ba5e3",
+    "url": "/static/media/about_background.82c705a4.png"
+  },
+  {
+    "revision": "6bad8033f40872410349d6ae066363bb",
+    "url": "/static/media/adam_fun.6bad8033.png"
+  },
+  {
+    "revision": "c6d695001991e21127599842000695b3",
+    "url": "/static/media/adam_serious.c6d69500.png"
+  },
+  {
+    "revision": "0c11c42e9d7c7b0282bc5c4a0d4773d8",
+    "url": "/static/media/caroline_serious.0c11c42e.png"
+  },
+  {
+    "revision": "01587f141bcc2a1fac35532f90536e2c",
+    "url": "/static/media/cassandra_fun.01587f14.png"
+  },
+  {
+    "revision": "820bf0e9f79aa170a3dfd265e930331b",
+    "url": "/static/media/cassandra_serious.820bf0e9.png"
+  },
+  {
+    "revision": "cb0c9cff50945187fc5c4658583bd5b4",
+    "url": "/static/media/charles_fun.cb0c9cff.png"
+  },
+  {
+    "revision": "f7e45cd016b19d1ebf2ac257cf854218",
+    "url": "/static/media/charles_serious.f7e45cd0.png"
+  },
+  {
+    "revision": "1bcc8278eedcc413a91f6960986d36e7",
+    "url": "/static/media/chase_serious.1bcc8278.png"
+  },
+  {
+    "revision": "713dfc09ed84c753d86aad498d348ac6",
+    "url": "/static/media/christian_fun.713dfc09.png"
+  },
+  {
+    "revision": "0d0f2873c4f32ea4278f4d3024993d00",
+    "url": "/static/media/christian_serious.0d0f2873.png"
+  },
+  {
+    "revision": "07feac8d414c0513001e3e8b71dd7064",
+    "url": "/static/media/class_photo_fun.07feac8d.png"
+  },
+  {
+    "revision": "723d6e1cd37e014ac19ddb5f49c7a0cd",
+    "url": "/static/media/class_photo_gradient.723d6e1c.png"
+  },
+  {
+    "revision": "259d2b6d5abe1cc348efde767ee7fce8",
+    "url": "/static/media/corri_fun.259d2b6d.png"
+  },
+  {
+    "revision": "d7e82b648df88beb8a938d983f3670eb",
+    "url": "/static/media/corri_serious.d7e82b64.png"
+  },
+  {
+    "revision": "6273d9a90781fe2a9e430d58fad1e7a0",
+    "url": "/static/media/erin_fun.6273d9a9.png"
+  },
+  {
+    "revision": "1e01a2db6f880a154fdb9b7938b7d0fe",
+    "url": "/static/media/erin_serious.1e01a2db.png"
+  },
+  {
+    "revision": "a891ab6c03bb1a75f2fb68a157846f0a",
+    "url": "/static/media/guy_fun.a891ab6c.png"
+  },
+  {
+    "revision": "cc550edab5da6c57d4d0070943ecad37",
+    "url": "/static/media/guy_serious.cc550eda.png"
+  },
+  {
+    "revision": "775845fa6ba8d2936b8516bb6edbd5f3",
+    "url": "/static/media/james_fun.775845fa.png"
+  },
+  {
+    "revision": "2602b577f1f88dcaed2b60cdad80de83",
+    "url": "/static/media/james_serious.2602b577.png"
+  },
+  {
+    "revision": "a4dd1a1c749f1dfa3eed30e385ce64f7",
+    "url": "/static/media/jeremiah_fun.a4dd1a1c.png"
+  },
+  {
+    "revision": "60016a1d0318e1bd2fa4452768ee4871",
+    "url": "/static/media/jeremiah_serious.60016a1d.png"
+  },
+  {
+    "revision": "e387399315362e751563b749c7b28712",
+    "url": "/static/media/jesie_serious.e3873993.png"
+  },
+  {
+    "revision": "369d7fa7e3b7f12a26c410915e94be59",
+    "url": "/static/media/john_fun.369d7fa7.png"
+  },
+  {
+    "revision": "3e6e4a6edee27904e9d0274bb4941f17",
+    "url": "/static/media/john_serious.3e6e4a6e.png"
+  },
+  {
+    "revision": "18cf967fcbf616a892f3eff4d557cf24",
+    "url": "/static/media/joseph_fun.18cf967f.png"
+  },
+  {
+    "revision": "561d36af1084b48340bfcb9352d817bb",
+    "url": "/static/media/joseph_serious.561d36af.png"
+  },
+  {
+    "revision": "6b483a84f41c3ce4d504ca5f7999b191",
+    "url": "/static/media/julian_fun.6b483a84.png"
+  },
+  {
+    "revision": "ab23b0116e8644f63a2374fb05156077",
+    "url": "/static/media/julian_serious.ab23b011.png"
+  },
+  {
+    "revision": "1b7b03d5f4d0ed9f83b216fecce85962",
+    "url": "/static/media/kenneth_fun.1b7b03d5.png"
+  },
+  {
+    "revision": "29c58b9354719e479d8d65432ff4e360",
+    "url": "/static/media/kenneth_serious.29c58b93.png"
+  },
+  {
+    "revision": "8f6f3c1f1e88750fd8f5e439e0cef5b2",
+    "url": "/static/media/lauren_fun.8f6f3c1f.png"
+  },
+  {
+    "revision": "17cfc107ab5aa7091ef3b01e176311c4",
+    "url": "/static/media/lauren_serious.17cfc107.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "63b1c37f0a59df24927948b850aeac53",
+    "url": "/static/media/manila_fun.63b1c37f.png"
+  },
+  {
+    "revision": "d46efbe67f3552051427564eed64bcdb",
+    "url": "/static/media/manila_serious.d46efbe6.png"
+  },
+  {
+    "revision": "f660527c3078f0766e1673c635c10ce7",
+    "url": "/static/media/matthew_fun.f660527c.png"
+  },
+  {
+    "revision": "907b2a3e6e1d09f018a03c2cc471897a",
+    "url": "/static/media/matthew_serious.907b2a3e.png"
+  },
+  {
+    "revision": "ab7d35dcf68cb1a7d0e26c19a4ddeec4",
+    "url": "/static/media/melody_fun.ab7d35dc.png"
+  },
+  {
+    "revision": "3a767eaa19e2b873208202ee9704f049",
+    "url": "/static/media/melody_serious.3a767eaa.png"
+  },
+  {
+    "revision": "cb7c6fbe2eb7233c658bc8814a724702",
+    "url": "/static/media/michelle_fun.cb7c6fbe.png"
+  },
+  {
+    "revision": "74bf4b9a48c307766b04f5badb7520cf",
+    "url": "/static/media/michelle_serious.74bf4b9a.png"
+  },
+  {
+    "revision": "6593e0a1f41d1e57bbd317194df23ad7",
+    "url": "/static/media/ryan_bishop_fun.6593e0a1.png"
+  },
+  {
+    "revision": "fa078a8418e3feb8b5d60c26adea0724",
+    "url": "/static/media/ryan_bishop_serious.fa078a84.png"
+  },
+  {
+    "revision": "f87bf67f815a9ea69798d43dc77db84c",
+    "url": "/static/media/ryan_crowley_fun.f87bf67f.png"
+  },
+  {
+    "revision": "2d9ff03e94e45a77a44a1b6707932b99",
+    "url": "/static/media/ryan_crowley_serious.2d9ff03e.png"
+  },
+  {
+    "revision": "88218fb20115bd5b9f461c216965012c",
+    "url": "/static/media/ryan_cunningham_fun.88218fb2.png"
+  },
+  {
+    "revision": "756bb42264b3fb12c939dc99ba209011",
+    "url": "/static/media/ryan_cunningham_serious.756bb422.png"
+  },
+  {
+    "revision": "e9daa2e198a7ffeb68d19d598b830ebc",
+    "url": "/static/media/sam_fun.e9daa2e1.png"
+  },
+  {
+    "revision": "4f6f65fc6330625a8ab9c82566ed5544",
+    "url": "/static/media/sam_serious.4f6f65fc.png"
+  },
+  {
+    "revision": "442889133de5c7b136744e318ed3fac8",
+    "url": "/static/media/shawna_fun.44288913.png"
+  },
+  {
+    "revision": "3626f1215ceea5999d0254d8f23644d5",
+    "url": "/static/media/shawna_serious.3626f121.png"
+  },
+  {
+    "revision": "bf0168656ea656b3428c860ef648288b",
+    "url": "/static/media/sophia_fun.bf016865.png"
+  },
+  {
+    "revision": "3ca7bbc2a0dcb647eda73c1cd0e0fb10",
+    "url": "/static/media/sophia_serious.3ca7bbc2.png"
+  },
+  {
+    "revision": "4e7265a136f01433404250d67db7c2ee",
+    "url": "/static/media/sullivan_fun.4e7265a1.png"
+  },
+  {
+    "revision": "3c962f2cfc0dcb2a3d43a654ab9c8b5e",
+    "url": "/static/media/sullivan_serious.3c962f2c.png"
+  },
+  {
+    "revision": "319a1d60a42bcdef4449fd7d79858ac8",
+    "url": "/static/media/thanks_background.319a1d60.png"
+  },
+  {
+    "revision": "e90912c0f509d12ede392c474bf095be",
+    "url": "/static/media/trey_fun.e90912c0.png"
+  },
+  {
+    "revision": "f88dfb846fe07583c8b876f908aa3559",
+    "url": "/static/media/trey_serious.f88dfb84.png"
+  }
+]);
